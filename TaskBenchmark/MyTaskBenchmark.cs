67fe3f64acb81d6f741d4bb2a9179bcd9aac5028
Mikhail Motlokhov <mmotlokhov@yandex.ru>
2021-12-25 19:50:21 +0300

    Added TaskWaitBenchMark

---
+++ TaskBenchmark/MyTaskBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Threading.Tasks;
+
+namespace TaskBenchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    public class MyTaskBenchmark
+    {
+        public readonly int TaskCount = 7;
+
+        private async Task<int> SomeAsyncTask(int i)
+        {
+            await Task.Delay(50);
+            return i;
+        }
+
+        private async Task CreateAwaitWhenAll(int taskCount)
+        {
+            Task<int>[] tasks = new Task<int>[taskCount];
+
+            for (int i = 0; i < taskCount; i++)
+            {
+                tasks[i] = SomeAsyncTask(i);
+            }
+
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task CreateAwaitEach(int taskCount)
+        {
+            for (int i = 0; i < taskCount; i++)
+            {
+                await SomeAsyncTask(i);
+            }
+        }
+
+        [Benchmark]
+        public async Task BenchGroup_1_Await_When_All() => await CreateAwaitWhenAll(TaskCount);
+
+        [Benchmark]
+        public async Task BenchGroup_1_Await_Each() => await CreateAwaitEach(TaskCount);
+    }
+}
---
+++ TaskBenchmark/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace TaskBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MyTaskBenchmark>();
+        }
+    }
+}
